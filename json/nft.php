6814d698f6b7c5da5444916b5473ee5f6f0a124e
nftredo <how2pages@gmail.com>
2024-05-19 21:52:16 +0900

    Update nft.php

--- json/nft.php
+++ json/nft.php
@@ -1,5 +1,5 @@
 <?php
-$nums = 99;
+$nums = 50;
 $i = 0; // Начинаем счет с 0
 
 while ($i <= $nums) {
@@ -13,8 +13,8 @@ while ($i <= $nums) {
 
     // Записываем данные в файл
     file_put_contents($filename, '{
-  "name": "Diablo Gato #' . $number . '",
-  "description": "The great, terrible and good friend of Diablo Gato",
+  "name": "REDO 2.0 #' . $number . '",
+  "description": "A homage to the creation of Pavel Durov, founder of TON - The Open Network and Telegram, Resistance Dog ($REDO), is a symbolic represtentation of our fight against censorship and freedom through digital resistance.",
   "content_url": "https://nftnova.org/nft/gato/v/' . $media_number . '.mp4",
   "image": "https://nftnova.org/nft/gato/i/' . $media_number . '.jpg",
   "attributes": [
